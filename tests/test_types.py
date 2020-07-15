7e6d387aa0374e41a5863f4a8230cff1ae58f8bc
tropicalmentat <gabtorres1011@gmail.com>
2020-07-15 08:33:00 +0800

    Added test for unicode fraction conversion to float. Needs further refinement

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,9 +1,10 @@
 import unittest
+import unicodedata
 from fractions import Fraction
 
 class TestSum(unittest.TestCase):
 	def test_fractions(self):
-		assert isinstance("¼",Fraction)
+		assert isinstance(unicodedata.numeric("¼"),float)
 
 if __name__=="__main__":
 	unittest.main()
